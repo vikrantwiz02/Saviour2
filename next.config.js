e53bac94fbd6bf7c8c70bdb27130977ab1d59fc9
vikrantwiz02 <vikrantkrd@gmail.com>
2024-12-24 12:44:54 +0530

    weather update 5

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any necessary configuration here, but avoid custom routing rules
+}
+
+module.exports = nextConfig
+
