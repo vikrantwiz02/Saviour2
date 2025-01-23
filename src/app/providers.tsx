edcc70cbea946ee561f1bdb97372c11e991de1ef
vikrantwiz02 <vikrantkrd@gmail.com>
2025-01-23 09:35:41 +0530

    revert 2

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,1 +1,14 @@
-revert
+'use client'
+
+import { SessionProvider } from "next-auth/react"
+import { Session } from "next-auth"
+
+type ProvidersProps = {
+  children: React.ReactNode
+  session: Session | null
+}
+
+export function Providers({ children, session }: ProvidersProps) {
+  return <SessionProvider session={session}>{children}</SessionProvider>
+}
+
